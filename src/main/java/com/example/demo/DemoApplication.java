609f28277ef71015e0364dc35f12691be1446690
ChoniGomez <chonidacharygomez@gmail.com>
2025-01-29 16:50:57 -0300

    Version Final

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,21 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		
+		/*
+		 * ingresar a la siguiente pagina desde el navegador
+		 * 
+		 * http://localhost:8080/demo/swagger-ui/index.html
+		 * 
+		 * cambiar la barra del explorer por la siguiente
+		 * 
+		 * /demo/v3/api-docs
+		 * 
+		 * para ver desde la pag web
+		 * 
+		 * http://localhost:8088/demo/
+		 * 
+		 * */
 	}
 
 }
